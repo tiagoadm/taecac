e5c9c200e3265c218e3cc10b5186502c8b650cbd
Tiago Marques <tadms@tuta.io>
2021-02-02 01:26:36 +0000

    Update online_retail_pg_data_model.sql

--- online_retail_pg_data_model.sql
+++ online_retail_pg_data_model.sql
@@ -1,40 +1,61 @@
---Índice para melhorar a performance das queries
-create index idx_uci_database_lkp on uci_database("InvoiceNo", "StockCode", "InvoiceDate");
-
---Corrigir o problema das minúsculas na coluna StockCode
-update public.uci_database
-set "StockCode" = UPPER("StockCode")
-
-/********* INICIO NORMALIZAÇÃO **********/
---Tabelas
-create table sales ("InvoiceNo" text, "StockCode" text, "CustomerID" float8, "InvoiceDate" timestamp, "UnitPrice" float8, "Quantity" int8,
-primary key ("InvoiceNo", "StockCode", "CustomerID"),
-CONSTRAINT fk_customer foreign key ("CustomerID") references customers("CustomerID"),
-CONSTRAINT fk_product foreign key ("StockCode") references products("StockCode")
-);
-create table products ("StockCode" text primary key, "Description" text, "Created" timestamp, "LastUpdate" timestamp);
-create table customers ("CustomerID" float8 primary key, "Country" text, "Created" timestamp, "LastUpdate" timestamp);
-
---Inserção de dados Customers
-insert into customers
-select distinct "CustomerID", 
-	first_value("Country") over (partition by "CustomerID" order by "InvoiceDate" desc), 
-	min("InvoiceDate") over (partition by "CustomerID"), 
-	max("InvoiceDate") over (partition by "CustomerID")
-from public.uci_database
-where "CustomerID" is not null;
---Inserção de dados Products
-insert into products
-select distinct "StockCode", 
-	first_value("Description") over (partition by "StockCode" order by "InvoiceDate" desc), 
-	min("InvoiceDate") over (partition by "StockCode"), 
-	max("InvoiceDate") over (partition by "StockCode")
-from public.uci_database;
---Inserção de dados Sales
-insert into sales
-select "InvoiceNo" , "StockCode" , "CustomerID" , max("InvoiceDate") , max("UnitPrice") "UnitPrice", sum("Quantity") "Quantity"
-FROM public.uci_database
-where "CustomerID" is not null
-group by "InvoiceNo" , "StockCode" , "CustomerID";
-
-/********* FIM NORMALIZAÇÃO **********/
+--Cleaned dataset
+create materialized view cleaned_dataset as 
+select distinct * --2.1
+from uci_database ud
+where left(upper("InvoiceNo"),1) not in ('A','C') --2.5
+and not "Description" ~ ('
+	wrong.*?|test|SAMPLES|Sale error|returned|on cargo order|Manual|mailout|incorrect.*?|
+	check|damage.*?|\?+|printing smudges/thrown away|crushed boxes|[Aa]djust.*?|[add|allocate] stock.*?|
+	[[Aa]mazon|AMAZON].*?|broken|Breakages|CARRIAGE|.*?[Dd]otcom.*?|[Ff]ound.*?|FOUND.*?|[Hh]ad been put aside.*?
+') --2.4
+and upper("StockCode") not like 'GIFT%' --2.4
+and "Quantity" > 0 --2.3
+and "UnitPrice" > 0 --2.3
+and "Description" is not null --2.2
+and "CustomerID" is not null --2.2
+and "InvoiceNo" is not null --2.2
+and "StockCode" is not null; --2.2
+--Transactions
+create table transactions as
+select distinct "InvoiceNo"
+    "StockCode",
+    "CustomerID",
+    "InvoiceDate",
+    "UnitPrice",
+    "Quantity"
+from cleaned_dataset;
+--Customers
+create table customers as
+with customers_intermediate as (
+    select distinct "CustomerID",
+        "Country",
+    "InvoiceDate",
+    max("InvoiceDate") over (partition by "CustomerID") as max_date
+    from cleaned_dataset
+)
+select distinct "CustomerID",
+    "Country",
+    "InvoiceDate" as "LastUpdate"
+from customers_intermediate
+where "InvoiceDate" = max_date; --3.2
+--Products
+create table products as
+with products_intermediate as (
+    select distinct "StockCode",
+        "Description",
+        "InvoiceDate",
+        max("InvoiceDate") over (partition by "StockCode") as max_date
+    from cleaned_dataset
+)
+select distinct "StockCode",
+    "Description",
+    "InvoiceDate" as "LastUpdate"
+from products_intermediate
+where "InvoiceDate" = max_date; --3.3
+--Primary keys
+ALTER TABLE transactions ADD PRIMARY KEY ("InvoiceNo", "StockCode", "CustomerID");
+ALTER TABLE customers ADD PRIMARY KEY ("CustomerID");
+ALTER TABLE products ADD PRIMARY KEY ("StockCode");
+--Foreign keys
+ALTER TABLE transactions ADD CONSTRAINT fk_prod FOREIGN KEY ("StockCode") REFERENCES products("StockCode");
+ALTER TABLE transactions ADD CONSTRAINT fk_cust FOREIGN KEY ("CustomerID") REFERENCES customers("CustomerID");
