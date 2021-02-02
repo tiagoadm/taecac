13048592de45ab2b2ebd97f3b0d2315ec20eccdd
Tiago Marques <tadms@tuta.io>
2021-02-02 01:25:49 +0000

    Rename online_retail_pg.sql to online_retail_pg_data_model.sql

--- online_retail_pg_data_model.sql
+++ online_retail_pg_data_model.sql
@@ -1,11 +1,11 @@
---Õndice para melhorar a performance das queries
+--√çndice para melhorar a performance das queries
 create index idx_uci_database_lkp on uci_database("InvoiceNo", "StockCode", "InvoiceDate");
 
---Corrigir o problema das min˙sculas na coluna StockCode
+--Corrigir o problema das min√∫sculas na coluna StockCode
 update public.uci_database
 set "StockCode" = UPPER("StockCode")
 
-/********* INICIO NORMALIZA«√O **********/
+/********* INICIO NORMALIZA√á√ÉO **********/
 --Tabelas
 create table sales ("InvoiceNo" text, "StockCode" text, "CustomerID" float8, "InvoiceDate" timestamp, "UnitPrice" float8, "Quantity" int8,
 primary key ("InvoiceNo", "StockCode", "CustomerID"),
@@ -15,7 +15,7 @@ CONSTRAINT fk_product foreign key ("StockCode") references products("StockCode")
 create table products ("StockCode" text primary key, "Description" text, "Created" timestamp, "LastUpdate" timestamp);
 create table customers ("CustomerID" float8 primary key, "Country" text, "Created" timestamp, "LastUpdate" timestamp);
 
---InserÁ„o de dados Customers
+--Inser√ß√£o de dados Customers
 insert into customers
 select distinct "CustomerID", 
 	first_value("Country") over (partition by "CustomerID" order by "InvoiceDate" desc), 
@@ -23,18 +23,18 @@ select distinct "CustomerID",
 	max("InvoiceDate") over (partition by "CustomerID")
 from public.uci_database
 where "CustomerID" is not null;
---InserÁ„o de dados Products
+--Inser√ß√£o de dados Products
 insert into products
 select distinct "StockCode", 
 	first_value("Description") over (partition by "StockCode" order by "InvoiceDate" desc), 
 	min("InvoiceDate") over (partition by "StockCode"), 
 	max("InvoiceDate") over (partition by "StockCode")
 from public.uci_database;
---InserÁ„o de dados Sales
+--Inser√ß√£o de dados Sales
 insert into sales
 select "InvoiceNo" , "StockCode" , "CustomerID" , max("InvoiceDate") , max("UnitPrice") "UnitPrice", sum("Quantity") "Quantity"
 FROM public.uci_database
 where "CustomerID" is not null
 group by "InvoiceNo" , "StockCode" , "CustomerID";
 
-/********* FIM NORMALIZA«√O **********/
+/********* FIM NORMALIZA√á√ÉO **********/
